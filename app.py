b455c55cf3c9971d3987e0f4629f8ecd10d96c7c
akraisinger <a.kraisinger88@gmail.com>
2024-02-22 14:32:19 -0500

    updated app.py

--- app.py
+++ app.py
@@ -13,20 +13,44 @@ transactions = [
 def get_transactions():
     return render_template('transactions.html', transactions=transactions)
 # Create operation
-@app.route("add", methods=['GET','POST'])
+@app.route("/add", methods=['GET','POST'])
 def add_transaction():
     if request.method == 'POST':
         transation = {
-              'id': len(transactions)+1
-              'date': request.form['date']
+              'id': len(transactions)+1,
+              'date': request.form['date'],
               'amount': float(request.form['amount'])
              }
         transactions.append(transaction)
         return redirect(url_for('get_transactions'))
     return render_template('form.html')
 # Update operation
+@app.route("/edit/<int:transaction_id>", methods=['GET','POST'])
+def edit_transaction(transaction_id):
+    if request.method == 'POST':
+        date = request.form['date']
+        amount = float(request.form['amount'])
+
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                transaction['date'] = date
+                transaction['amount'] = amount
+                break
+        return redirect(url_for('get_transactions'))
+    
+    for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                return render_template('edit.html', transaction=transaction)
 
 # Delete operation
+@app.route("/delete/<int:transaction_id>", method=['DELETE'])
+def delete_transaction(transaction_id):
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            transactions.remove(transaction)
+            break
+    return redirect(url_for('get_transactions'))
 
 # Run the Flask app
-    
+if __name__ == "__main__":
+    app.run(debug=True)
